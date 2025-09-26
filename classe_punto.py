b9ccc3831b85363580ff3f38e32ea8769c2776dc
Fabio Monaco <fabiomonaco007@gmail.com>
2025-09-26 09:48:51 +0200

    Update classe_punto.py

--- classe_punto.py
+++ classe_punto.py
@@ -15,6 +15,23 @@ class Punto:
     
     def visualizza(self):
         print(f"({self.x}, {self.y})")
+        
+class Rettangolo:
+    def __init__(self, punto1: Punto, punto2: Punto):
+        self.punto1 = punto1
+        self.punto2 = punto2
+        
+    def get_base(self):
+        base = abs(self.punto1.x - self.punto2.x)
+        return base 
+    
+    def get_altezza(self):
+        altezza = abs(self.punto1.y - self.punto2.y)
+        return altezza
+    
+    def get_area(self):
+        area = self.get_base() * self.get_altezza()
+        return area
 
 punto1 = Punto(3, 4)
 punto2 = Punto(6, 8)
@@ -24,3 +41,7 @@ punto2.visualizza()
 
 calcola_distanza = punto1.calcola_distanza_punto(punto2) # funzione che calcola la distanza tra i due punti
 print(f"La distanza tra i due punti è: {calcola_distanza}")
+
+r1 = Rettangolo(punto1, punto2)
+print(r1.get_base())
+print(r1.get_altezza())
